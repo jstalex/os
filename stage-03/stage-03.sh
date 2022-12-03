edcd9956b57d1ef2f9bc226d8ac468646e99aba7
jstalex <a1s3rp.v@gmail.com>
2022-12-03 14:51:52 +0300

    added symbol reading from keypad

--- stage-03/stage-03.sh
+++ stage-03/stage-03.sh
@@ -2,9 +2,21 @@
 
 ./stage-03-01.sh &
 
+pass=""
+
 while true
 do
-    if read pass && [[ "$pass" == '1337' ]]; then
+    while read sym && [[ "$sym" != '*' ]]
+    do
+        pass+="$sym"
+        echo "$pass"
+    done
+
+    if [[ "$pass" == '1337' ]]; then
         echo "Successful entry" >> result.txt
+    fi
+
+    if [[ "$sym" == '*' ]]; then
+        pass=""
     fi
 done <"keypad_data"
